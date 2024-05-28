2fb07b00efb8ecf422a2801e03212f6068b7e253
daniarustom <daniarustom3@gmail.com>
2024-05-28 00:04:19 -0400

    checkpoint

--- main.py
+++ main.py
@@ -70,6 +70,7 @@ class RubiksCubeEnv(gym.Env):
             'U': np.array([[[0, 0, 0, 0, 1, 0] for _ in range(3)] for _ in range(3)], dtype=np.uint8),  # Blue
             'D': np.array([[[0, 0, 0, 0, 0, 1] for _ in range(3)] for _ in range(3)], dtype=np.uint8)  # Green
         }
+
         return cube
 
     def reset(self, seed=None):
